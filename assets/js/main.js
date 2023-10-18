9cea41cfd706a97a4127dec0821a129963faa829
shivtej <shivtej.img@gmail.com>
2023-10-18 09:03:36 +0530

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,27 @@ function scrollFunction() {
         document.getElementById("navbar").classList.remove('fixed-navbar');
     }
 }
+
+// awared section 
+
+$('.awaredOwl').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    dots:false,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
